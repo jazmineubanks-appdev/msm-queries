dd4c37eec602ed845515e6fc61c9d968cc3e6447
Jazmin Eubanks <jazmin.eubanks@gmail.com>
2023-04-12 20:07:02 +0000

    40_percent

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+
+  get("/directors/eldest", { :controller => "directors", :action => "wisest"})
+
+  get("/directors/youngest", { :controller => "directors", :action => "least_wise"})
+
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors", { :controller => "directors", :action => "index"})
 
-  get("/directors/eldest", { :controller => "directors", :action => "wisest"})
+ get("/directors/:an_id", { :controller => "directors", :action => "director_details"})
+
+
+
 end
